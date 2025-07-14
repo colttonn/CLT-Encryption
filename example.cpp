fbe25822c04015872d9692ce10303af43bb1985d
Colton <colttonn1337@gmail.com>
2025-07-14 18:23:34 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -11,4 +11,4 @@ int main() {
     return 0;
 }
 
-//Credits to https://github.com/skadro-official/skCrypter and https://github.com/llxiaoyuan/oxorany
+//Credits to https://github.com/skadro-official/skCrypter and https://github.com/llxiaoyuan/oxorany and https://github.com/openluopworld/aes_128/
